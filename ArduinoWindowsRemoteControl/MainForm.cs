e4a8e7477178e300f8a7ba9f2d0b247544e86e1b
Stanislav Ushakov <stasushakov@gmail.com>
2015-02-17 18:57:27 +0300

    Added WindowsKeyboardCommand

--- ArduinoWindowsRemoteControl/MainForm.cs
+++ ArduinoWindowsRemoteControl/MainForm.cs
@@ -16,5 +16,10 @@ namespace ArduinoWindowsRemoteControl
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArduinoWindowsRemoteControl.Helpers.WinAPIHelpers.SendKeyboardMessage("ALT-TAB-TAB");
+        }
     }
 }
---
+++ ArduinoWindowsRemoteControl/Windows/WindowsKeyboardCommand.cs
@@ -0,0 +1,37 @@
+﻿using ArduinoWindowsRemoteControl.Helpers;
+using ArduinoWindowsRemoteControl.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoWindowsRemoteControl.Windows
+{
+    public class WindowsKeyboardCommand : ICommand
+    {
+        #region Private Fields
+
+        private string _keyboardcommand;
+
+        #endregion
+
+        #region Constructor
+
+        public WindowsKeyboardCommand(string keyboardCommand)
+        {
+            _keyboardcommand = keyboardCommand;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Do()
+        {
+            WinAPIHelpers.SendKeyboardMessage(_keyboardcommand);
+        }
+
+        #endregion
+    }
+}
