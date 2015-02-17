7c861b50298915b74caec1909c3ef0a74a395caf
Stanislav Ushakov <stasushakov@gmail.com>
2015-02-17 17:48:23 +0300

    Added ability to get current process name

---
+++ ArduinoWindowsRemoteControl/Helpers/WinAPIHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoWindowsRemoteControl.Helpers
+{
+    public static class WinAPIHelpers
+    {
+        /// <summary>
+        /// Returns current active application name
+        /// </summary>
+        /// <returns>Current active application name</returns>
+        public static string GetActiveApplicationName()
+        {
+            IntPtr hWnd = WinAPIMethods.GetForegroundWindow();
+            uint procId = 0;
+            WinAPIMethods.GetWindowThreadProcessId(hWnd, out procId);
+            var process = Process.GetProcessById((int)procId);
+
+            return process.ProcessName;
+        }
+    }
+}
