d19a5f1ac28ef1ea577d684ef9fe5218834b5b20
Stanislav Ushakov <stasushakov@gmail.com>
2015-02-18 11:34:21 +0300

    Added ICommandFactory

---
+++ ArduinoWindowsRemoteControl/Interfaces/ICommandFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoWindowsRemoteControl.Interfaces
+{
+    /// <summary>
+    /// Factory for creating ICommand objects
+    /// </summary>
+    public interface ICommandFactory
+    {
+        /// <summary>
+        /// Creates an instance of ICommand
+        /// </summary>
+        /// <param name="command">Command specification</param>
+        /// <returns>ICommand object</returns>
+        ICommand Create(string command);
+    }
+}
