6c7b24b329974b77821cfe44fc52d98f7d68dd79
Stanislav Ushakov <stasushakov@gmail.com>
2015-02-20 16:26:44 +0300

    Added all remote commands

--- ArduinoWindowsRemoteControl/Interfaces/IArduinoDevice.cs
+++ ArduinoWindowsRemoteControl/Interfaces/IArduinoDevice.cs
@@ -1,20 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ArduinoWindowsRemoteControl.Interfaces
 {
+    #region RemoteCommand Enum
+
     /// <summary>
     /// Represents received remote command
     /// </summary>
     public enum RemoteCommand
     {
-        TurnOn,
-        TurnOf, 
-        Play
+        [Description("Turn On/Off")]
+        TurnOnOff,
+
+        [Description("Mode")]
+        Mode,
+
+        [Description("Mute")]
+        Mute,
+
+        [Description("Play/Paus")]
+        PlayPause,
+
+        [Description("Previous")]
+        Previous,
+
+        [Description("Next")]
+        Next,
+
+        [Description("EQ")]
+        EQ,
+
+        [Description("Minus")]
+        Minus,
+
+        [Description("Plus")]
+        Plus,
+
+        [Description("Random")]
+        Random,
+
+        [Description("U/SD")]
+        USD,
+
+        [Description("0")]
+        Dig0,
+
+        [Description("1")]
+        Dig1,
+
+        [Description("2")]
+        Dig2,
+
+        [Description("3")]
+        Dig3,
+
+        [Description("4")]
+        Dig4,
+
+        [Description("5")]
+        Dig5,
+
+        [Description("6")]
+        Dig6,
+
+        [Description("7")]
+        Dig7,
+
+        [Description("8")]
+        Dig8,
+
+        [Description("9")]
+        Dig9
     }
+
+    #endregion
 
     /// <summary>
     /// Represents device that is connected with the remote control
