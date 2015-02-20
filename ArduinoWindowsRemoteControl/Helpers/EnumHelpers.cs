e40e1894d37df26ecec472dd013563398b76a5f6
Stanislav Ushakov <stasushakov@gmail.com>
2015-02-20 16:29:15 +0300

    Comments added

--- ArduinoWindowsRemoteControl/Helpers/EnumHelpers.cs
+++ ArduinoWindowsRemoteControl/Helpers/EnumHelpers.cs
@@ -13,14 +13,20 @@ namespace ArduinoWindowsRemoteControl.Helpers
         /// <summary>
         /// Get user friendly enum value description
         /// </summary>
-        /// <param name="enumValue"></param>
-        /// <returns></returns>
+        /// <param name="enumValue">Enum value</param>
+        /// <returns>String representation</returns>
         public static string ToDisplayName(this Enum enumValue)
         {
             var displayNameAttribute = enumValue.GetType().GetField(enumValue.ToString()).GetAttribute<DescriptionAttribute>();
             return displayNameAttribute != null ? displayNameAttribute.Description : enumValue.ToString();
         }
 
+        /// <summary>
+        /// Returns specific attribute (via reflection)
+        /// </summary>
+        /// <typeparam name="T">Type of attribute</typeparam>
+        /// <param name="member">Member to retrieve attribute</param>
+        /// <returns>Attribute value</returns>
         public static T GetAttribute<T>(this MemberInfo member)
         {
             return member.GetCustomAttributes(typeof(T), true).Cast<T>().SingleOrDefault();
