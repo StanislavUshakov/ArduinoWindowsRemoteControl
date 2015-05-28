66ebbfb34b8f7e10811a482cfe1b346713f1e8b0
Stanislav Ushakov <stasushakov@gmail.com>
2015-05-28 17:02:34 +0300

    Added stub for Arduino device

---
+++ ArduinoWindowsRemoteControl/SelectPortForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ArduinoWindowsRemoteControl
+{
+    partial class SelectPortForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbSerialPorts = new System.Windows.Forms.ComboBox();
+            this.lbSelectPort = new System.Windows.Forms.Label();
+            this.btOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbSerialPorts
+            // 
+            this.cbSerialPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSerialPorts.FormattingEnabled = true;
+            this.cbSerialPorts.Location = new System.Drawing.Point(12, 27);
+            this.cbSerialPorts.Name = "cbSerialPorts";
+            this.cbSerialPorts.Size = new System.Drawing.Size(260, 21);
+            this.cbSerialPorts.TabIndex = 0;
+            // 
+            // lbSelectPort
+            // 
+            this.lbSelectPort.AutoSize = true;
+            this.lbSelectPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbSelectPort.Location = new System.Drawing.Point(12, 8);
+            this.lbSelectPort.Name = "lbSelectPort";
+            this.lbSelectPort.Size = new System.Drawing.Size(75, 16);
+            this.lbSelectPort.TabIndex = 1;
+            this.lbSelectPort.Text = "Select port:";
+            // 
+            // btOk
+            // 
+            this.btOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btOk.Location = new System.Drawing.Point(197, 54);
+            this.btOk.Name = "btOk";
+            this.btOk.Size = new System.Drawing.Size(75, 23);
+            this.btOk.TabIndex = 2;
+            this.btOk.Text = "Connect";
+            this.btOk.UseVisualStyleBackColor = true;
+            this.btOk.Click += new System.EventHandler(this.btOk_Click);
+            // 
+            // SelectPortForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 87);
+            this.ControlBox = false;
+            this.Controls.Add(this.btOk);
+            this.Controls.Add(this.lbSelectPort);
+            this.Controls.Add(this.cbSerialPorts);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SelectPortForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Select serial port with Arduino";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbSerialPorts;
+        private System.Windows.Forms.Label lbSelectPort;
+        private System.Windows.Forms.Button btOk;
+    }
+}
---
+++ ArduinoWindowsRemoteControl/SelectPortForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoWindowsRemoteControl
+{
+    /// <summary>
+    /// This form is used only for selecting serial port
+    /// </summary>
+    public partial class SelectPortForm : Form
+    {
+        #region Constructor
+
+        public SelectPortForm()
+        {
+            InitializeComponent();
+
+            cbSerialPorts.Items.AddRange(SerialPort.GetPortNames());
+            cbSerialPorts.SelectedIndex = 0;
+        }
+
+        #endregion
+
+        #region Private Event Handlers
+
+        private void btOk_Click(object sender, EventArgs e)
+        {
+            Program.PortName = cbSerialPorts.SelectedItem as string; ;
+            Close();
+        }
+
+        #endregion
+    }
+}
