89f5b53c2b9eb64a290a6519239cd00396236d17
Stanislav Ushakov <stasushakov@gmail.com>
2015-05-28 18:33:21 +0300

    Added repository for saving Dictionary

---
+++ ArduinoWindowsRemoteControl/Repositories/DictionaryRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoWindowsRemoteControl.Repositories
+{
+    public class DictionaryRepository
+    {
+        #region Private Constants
+
+        private const string KeyValueDelimeter = ":::";
+        private const string ItemsDelimeter = "\r\n;;;\r\n";
+
+        #endregion
+
+        #region Public Methods
+
+        public void Save<TKey, TValue>(Dictionary<TKey, TValue> dictionary, string filename)
+        {
+            using (var writer = new StreamWriter(filename, false))
+            {
+                foreach (var kvp in dictionary)
+                {
+                    writer.Write(kvp.Key.ToString());
+                    writer.Write(KeyValueDelimeter);
+                    writer.Write(kvp.Value.ToString());
+                    writer.Write(ItemsDelimeter);
+                }
+            }
+        }
+
+        public Dictionary<TKey, TValue> Load<TKey, TValue>(string filename)
+        {
+            var result = new Dictionary<TKey, TValue>();
+
+            using (var reader = new StreamReader(filename))
+            {
+                var lines = reader.ReadToEnd();
+                var items = lines.Split(new string[] { ItemsDelimeter }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var item in items)
+                {
+                    var parts = item.Split(new string[] { KeyValueDelimeter }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    result.Add((TKey)Convert.ChangeType(parts[0], typeof(TKey)), (TValue)Convert.ChangeType(parts[1], typeof(TValue)));
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ UnitTests/DictionaryRepositoryUnitTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ArduinoWindowsRemoteControl.Repositories;
+using System.IO;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class DictionaryRepositoryUnitTest
+    {
+        [TestMethod]
+        public void TestSaveAndLoadEmpty()
+        {
+            var dictionary = new Dictionary<int, string>();
+            var repository = new DictionaryRepository();
+            string filename = "test.txt";
+            repository.Save(dictionary, filename);
+            Assert.IsTrue(File.Exists(filename));
+
+            var loadedDictionary = repository.Load<int, string>(filename);
+
+            Assert.AreEqual(loadedDictionary.Count, 0);
+        }
+
+        [TestMethod]
+        public void TestSaveAndLoadOneElement()
+        {
+            var dictionary = new Dictionary<int, string>() { {1, "test"} };
+            var repository = new DictionaryRepository();
+            string filename = "test.txt";
+            repository.Save(dictionary, filename);
+            Assert.IsTrue(File.Exists(filename));
+
+            var loadedDictionary = repository.Load<int, string>(filename);
+
+            Assert.AreEqual(loadedDictionary.Count, 1);
+            Assert.AreEqual(loadedDictionary[1], "test");
+        }
+
+        [TestMethod]
+        public void TestSaveAndLoadMany()
+        {
+            var dictionary = new Dictionary<int, string>() { { 1, "test" }, { 0, "a" }, { -1, "b" } };
+            var repository = new DictionaryRepository();
+            string filename = "test.txt";
+            repository.Save(dictionary, filename);
+            Assert.IsTrue(File.Exists(filename));
+
+            var loadedDictionary = repository.Load<int, string>(filename);
+
+            Assert.AreEqual(loadedDictionary.Count, 3);
+            Assert.AreEqual(loadedDictionary[1], "test");
+            Assert.AreEqual(loadedDictionary[0], "a");
+            Assert.AreEqual(loadedDictionary[-1], "b");
+        }
+    }
+}
